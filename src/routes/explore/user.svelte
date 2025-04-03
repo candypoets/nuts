0d816d0cc4ea432e57ba952a0548462cb3112277
sotachi <sotachi@proton.me>
2025-04-03 13:10:11 +0100

    fetch user if missing in context

--- src/routes/explore/user.svelte
+++ src/routes/explore/user.svelte
@@ -1,19 +1,42 @@
 <script lang="ts">
-	import type { AnyKind, Kind0Parsed } from 'src/parsers';
+	import { isKind0, type AnyKind, type Kind0Parsed } from 'src/parsers';
+	import { nostrManager, type EventKind } from 'src/wasm/manager';
 	import type { ParsedEvent } from 'src/workers/nipworker';
+	import { onDestroy } from 'svelte';
 
 	export let pubkey: string;
 	export let link: boolean = true;
+	export let relays: string[] = [];
 	export let context: ParsedEvent<AnyKind>[];
+	export let query = true;
 
 	let user: Kind0Parsed | undefined;
+	let sub: () => void;
 
 	$: {
 		if (!user && pubkey) {
 			user = (context || []).find((event) => event.kind === 0 && event.pubkey === pubkey)
 				?.parsed as Kind0Parsed | undefined;
+			if (!user && query) {
+				sub = nostrManager.subscribe(
+					'user_' + pubkey,
+					[{ kinds: [0], authors: [pubkey], limit: 1, cacheFirst: true, relays }],
+					(events: ParsedEvent<AnyKind>[], type: EventKind) => {
+						const [event, ...context] = events;
+						if (isKind0(event)) {
+							user = event.parsed as Kind0Parsed;
+						}
+					}
+				);
+			}
 		}
 	}
+
+	onDestroy(() => {
+		if (sub) {
+			sub();
+		}
+	});
 </script>
 
 {#if link}
