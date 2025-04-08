54a56276a6a0c457d459b143b48f9af12e38ecc7
sotachi <sotachi@proton.me>
2025-04-08 15:01:02 +0100

    Enhance feed component with loading state

--- src/routes/explore/feed.svelte
+++ src/routes/explore/feed.svelte
@@ -46,12 +46,14 @@
 
 	let eose = false;
 	let eoce = false;
+	let loading = true;
 	// Combined feed including the header item if provided
 	$: combinedItems = headerItem ? [headerItem, ...feed] : feed;
 
 	// In a separate function to avoid infinite loops in the reactive block
 	const handleEvents = (events: ParsedEvent<AnyKind>[], eventKind: SubscribeKind) => {
 		if (eventKind == 'EOSE' && !eose) {
+			loading = false;
 			eose = true;
 			feed = _.uniqBy([...fetchedFeed, ...feed], (item) => item[0].id)
 				.slice(0, 100)
@@ -95,7 +97,6 @@
 	$: {
 		if (requests && requests.length) {
 			timeout = setTimeout(() => {
-				console.log('SUBSCRIBING: ', subscriptionID, requests);
 				eoce = false;
 				eose = false;
 				cachedFeed = [];
@@ -114,9 +115,7 @@
 				lastBufferDump = now();
 			}
 		}, 2000);
-		// nostrManager.subscribe(subscriptionID, requests, handleEvents);
 		return () => {
-			console.log('UNSUBSCRIBING: ', subscriptionID, requests);
 			sub?.();
 			clearTimeout(timeout);
 			clearInterval(interval);
@@ -131,17 +130,6 @@
 		// goto('/explore');
 	}}
 >
-	<!-- <button
-		class="p-4 bg-primary rounded-full text-white fixed bottom-28 lg:bottom-4 lg:right-1/3 right-4 z-10 border-opacity-0 lg:translate-x-20"
-		class:bg-opacity-10={fadein}
-		on:click={(e) => {
-			e.stopPropagation();
-			$posting = true;
-		}}
-	>
-		<Icon icon="teenyicons:add-outline" class="text-2xl" />
-	</button> -->
-
 	<div class="absolute top-6 w-full z-40" transition:fly={{ y: -50, duration: 300 }}>
 		{#if newPosts.length}
 			<div
@@ -161,7 +149,7 @@
 	{#if start >= 1}
 		<!-- Fixed header (only visible when scrolled) -->
 		<div class="absolute z-10 w-full">
-			<div class="w-feed m-auto">
+			<div class="w-feed m-auto" on:click={() => viewport.scrollTo({ top: 0, behavior: 'smooth' })}>
 				<slot name="sticky-header" visible={true} scrolled={true} />
 			</div>
 		</div>
@@ -176,11 +164,6 @@
 		let:item
 	>
 		{#if headerItem && item.id == headerItem?.id}
-			<!-- Regular header in normal document flow (only visible at the top) -->
-			<!-- <div class="relative w-feed m-auto">
-					<slot name="sticky-header" visible={true} scrolled={false} />
-				</div> -->
-
 			<!-- Render header item -->
 			<div
 				class="block w-feed lg:m-auto py-1 px-1 max-w-full"
@@ -188,7 +171,7 @@
 				id="header"
 			>
 				<!-- Render custom header content -->
-				<slot name="header-content" {item} visible={start < 1} />
+				<slot name="header-content" {item} />
 			</div>
 		{:else}
 			{@const post = item[0]}
@@ -200,7 +183,7 @@
 				</slot>
 			</div>
 		{/if}
-		{#if !feed.length}
+		{#if loading}
 			<div
 				class="lg:pt-0 overflow-scroll scrollbar-hide container-height lg:container-height m-auto w-feed !pt-0"
 			>
