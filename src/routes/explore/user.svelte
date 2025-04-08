02f250853bd846ae241576edfccc32e93d90464b
sotachi <sotachi@proton.me>
2025-04-08 15:01:19 +0100

    Add user profile navigation

--- src/routes/explore/user.svelte
+++ src/routes/explore/user.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import { isKind0, type AnyKind, type Kind0Parsed } from 'src/parsers';
 	import { nostrManager, type EventKind } from 'src/wasm/manager';
 	import type { ParsedEvent } from 'src/workers/nipworker';
@@ -37,11 +39,23 @@
 			sub();
 		}
 	});
+
+	function go() {
+		const currentPath = $page.url.pathname;
+		const profilePath = `nprofile:${pubkey}`;
+
+		// Check if the current URL already ends with the profile we're trying to navigate to
+		if (!currentPath.endsWith(profilePath)) {
+			goto(`${currentPath}/${profilePath}`);
+		}
+	}
 </script>
 
 {#if link}
-	<strong class="text-violet-700 whitespace-nowrap"
-		>@{user?.name?.trim() || pubkey?.slice(0, 15) + '...'}</strong
+	<a
+		class="text-violet-700 whitespace-nowrap hover:underline"
+		on:click|stopPropagation|preventDefault={go}
+		>@{user?.name?.trim() || pubkey?.slice(0, 15) + '...'}</a
 	>
 {:else}
 	<span>{user?.name?.trim() || pubkey?.slice(0, 15) + '...'}</span>
