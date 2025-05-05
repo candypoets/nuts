c5baf57db6eae7d4cd0c5b72caa5d16ec1f765b8
sotachi <sotachi@proton.me>
2025-05-05 14:52:48 +0200

    Refactor MintSelector to use wallet controller

--- src/comp/MintSelector.svelte
+++ src/comp/MintSelector.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { getAmountAvailable, mint, mints } from 'src/stores/mints';
-	import { formatAmount } from 'src/actions/wallet';
+	import Icon from '@iconify/svelte';
+	import { mints, balanceByMint, activeMintUrl, mint } from 'src/controller/wallet';
 
 	const formatMintText = (url: string) => {
 		if (url.length <= 20) {
@@ -10,8 +10,6 @@
 		const last10 = url.substring(url.length - 10, url.length);
 		return `${first10}...${last10}`;
 	};
-
-	$: console.log($mints);
 </script>
 
 <button class="dropdown dropdown-bottom w-full my-2">
@@ -20,31 +18,15 @@
 	<label tabindex="0" class="join-item w-full">
 		<div class="flex items-center justify-between gap-1 w-full px-4">
 			<div class="rounded-full bg-success w-4 h-4 p-0.5">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="text-white"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-				>
-					<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-				</svg>
+				<Icon class="text-white" icon="mdi:check" width="100%" height="100%"></Icon>
 			</div>
 			<p class="max-w-xs text-xs flex-grow">
-				{$mint?.mintURL ?? '----'}
+				{#await $mint then m}
+					{m?.name ?? '----'}
+				{/await}
 			</p>
 			<div>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-				</svg>
+				<Icon icon="mdi:chevron-down" class="w-6 h-6" />
 			</div>
 		</div>
 	</label>
@@ -54,35 +36,26 @@
 		tabindex="0"
 		class="z-10 dropdown-content menu py-2 shadow bg-base-100 rounded-box md:w-72 max-h-56 overflow-scroll flex-row scrollbar-hide w-full"
 	>
-		{#each $mints as m}
-			<li on:click={() => ($mint = m)} class="rounded-xl w-full">
-				<div class="flex gap-1 items-center w-full">
-					{#if m.mintURL === $mint?.mintURL}
-						<div class="rounded-full bg-success w-4 h-4 p-0.5">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="text-white"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-							>
-								<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-							</svg>
+		{#await $mints then mintsArray}
+			{#each mintsArray as m}
+				<li on:click={() => ($activeMintUrl = m.url)} class="rounded-xl w-full">
+					<div class="flex gap-1 items-center w-full">
+						{#if m.url === $activeMintUrl}
+							<div class="rounded-full bg-success w-4 h-4 p-0.5">
+								<Icon class="text-white" icon="mdi:check" width="100%" height="100%"></Icon>
+							</div>
+						{:else}
+							<div class="w-4 h-4" />
+						{/if}
+						<a class="flex-grow text-xs">{m.name}</a>
+						<div class="flex gap-1 items-center w-20 justify-end">
+							<p class="font-bold">
+								{$balanceByMint[m.url] || 0}
+							</p>
 						</div>
-					{:else}
-						<div class="w-4 h-4" />
-					{/if}
-					<a class="flex-grow text-xs">{m.mintURL}</a>
-					<div class="flex gap-1 items-center w-20 justify-end">
-						<p class="font-bold">
-							{#await getAmountAvailable(m) then amount}
-								{formatAmount(amount, 'sat')}
-							{/await}
-						</p>
 					</div>
-				</div>
-			</li>
-		{/each}
+				</li>
+			{/each}
+		{/await}
 	</ul>
 </button>
