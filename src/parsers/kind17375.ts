1482d51f54f78d4653ab9a145effd52826a13f40
sotachi <sotachi@proton.me>
2025-05-06 11:17:49 +0200

    align js parser with go parser

--- src/parsers/kind17375.ts
+++ src/parsers/kind17375.ts
@@ -3,6 +3,7 @@ import type { NostrEvent } from 'nostr-tools';
 export type Kind17375Parsed = {
 	mints: string[]; // List of mint URLs
 	p2pkPrivKey?: string; // Private key for P2PK ecash (if decrypted)
+	p2pkPubKey?: string;
 	decrypted: boolean; // Whether content was successfully decrypted
 };
 
