a9f5e22dad1f6b16f74f97d1dab3f9d8d3aaf514
sotachi <sotachi@proton.me>
2025-07-04 12:40:37 +0200

    Add theme persistence and color scheme support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,17 @@
 	import { pwaInfo } from 'virtual:pwa-info';
 	import App from 'src/routes/index.svelte';
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
 	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+
+	onMount(() => {
+		let theme = localStorage.getItem('theme') || 'dark';
+		document.getElementsByTagName('html')[0].setAttribute('data-theme', theme);
+		document
+			.querySelector('meta[name="theme-color"]')
+			?.setAttribute('content', theme === 'dark' ? '#131716' : '#f9fafb');
+	});
 </script>
 
 <svelte:head>
