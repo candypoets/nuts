8340b192d4168d14bc4de11b263cb861a4a4a146
sotachi <sotachi@proton.me>
2025-07-15 12:43:34 +0200

    Split user query into separate subscription filters

--- src/routes/queries/user.ts
+++ src/routes/queries/user.ts
@@ -1,8 +1,24 @@
 export const userQuery = (pubkey: string, relays: string[] = []) => [
 	{
-		kinds: [0, 10002, 10019],
+		kinds: [0],
 		authors: [pubkey],
-		limit: 5,
+		limit: 1,
+		cacheFirst: true,
+		closeOnEOSE: true,
+		relays
+	},
+	{
+		kinds: [10002],
+		authors: [pubkey],
+		limit: 1,
+		cacheFirst: true,
+		closeOnEOSE: true,
+		relays
+	},
+	{
+		kinds: [10019],
+		authors: [pubkey],
+		limit: 1,
 		cacheFirst: true,
 		closeOnEOSE: true,
 		relays
