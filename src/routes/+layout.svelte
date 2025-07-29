f297795503eb7044050d09f850634ea957361530
sotachi <sotachi@proton.me>
2025-07-29 12:56:58 +0200

    Add viewport sizing and resize event handling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,38 @@
 	import App from 'src/routes/index.svelte';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import { dimensions } from 'src/controller';
 
 	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 
+	function setViewport() {
+		document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+		document.documentElement.style.setProperty(
+			'--vw',
+			`${document.documentElement.clientWidth * 0.01}px`
+		);
+
+		$dimensions.width = window.innerWidth;
+		$dimensions.height = window.innerHeight;
+	}
+
 	onMount(() => {
 		let theme = localStorage.getItem('theme') || 'dark';
+		setViewport();
 		document.getElementsByTagName('html')[0].setAttribute('data-theme', theme);
 		document
 			.querySelector('meta[name="theme-color"]')
 			?.setAttribute('content', theme === 'dark' ? '#131716' : '#f9fafb');
+
+		window.addEventListener('resize', setViewport);
+		// Visual Viewport API for better keyboard detection
+		if (window.visualViewport) {
+			window.visualViewport.addEventListener('resize', setViewport);
+		}
+
+		return () => {
+			window.removeEventListener('resize', setViewport);
+		};
 	});
 </script>
 
