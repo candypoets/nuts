527245c4d4aec937de4fbc3afa28205a47969c19
sotachi <sotachi@proton.me>
2025-08-02 21:51:43 +0200

    Switch from static to node adapter for SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
-// import adapter from "@sveltejs/adapter-auto";
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({ fallback: 'index.html' }), // may differ from host to host
+		adapter: adapter({
+			out: 'build'
+		}),
 		serviceWorker: {
 			register: false
 		}
-		// ssr: false
 	}
 };
 
